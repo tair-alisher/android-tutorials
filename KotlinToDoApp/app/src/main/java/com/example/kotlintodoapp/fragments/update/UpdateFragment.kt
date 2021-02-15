3a2b394d5844c6036c918e97087b4c8fcb8b568e
Tairakhunov <ur.alisher@mail.ru>
2021-02-15 21:38:04 +0600

    Menu for update fragment added.

--- KotlinToDoApp/app/src/main/java/com/example/kotlintodoapp/fragments/update/UpdateFragment.kt
+++ KotlinToDoApp/app/src/main/java/com/example/kotlintodoapp/fragments/update/UpdateFragment.kt
@@ -1,10 +1,8 @@
 package com.example.kotlintodoapp.fragments.update
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.kotlintodoapp.R
 
 class UpdateFragment : Fragment() {
@@ -12,7 +10,12 @@ class UpdateFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        setHasOptionsMenu(true)
+
         return inflater.inflate(R.layout.fragment_update, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.update_fragment_menu, menu)
     }
 }
